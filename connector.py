9d7dcd9bccccd6ee0caf7f505ac6ef7de19ac952
Jamie <ScruffyTheMoose@users.noreply.github.com>
2022-07-05 18:21:26 -0600

    begin restful build

--- connector.py
+++ connector.py
@@ -1,16 +1,25 @@
-import getpass
-from mysql.connector import connect, Error
+# https://www.opentechguides.com/how-to/article/python/210/flask-mysql-crud.html
 
+from getpass import getpass, getuser
 
-# initializing connection to the SP500 database
-try:
-    cnx = connect(
-        host="localhost",
-        user=input("Username: "),
-        password=getpass("Password: "),
-        database="sp500_db",
-    )
-except Error as err:
-    print(err)
+from flask import Flask, jsonify, request
+from flaskext.mysql import MySQL
+from flask_restful import Resource, Api
 
-cursor = cnx.cursor()
+# init flask instance
+app = Flask(__name__)
+
+# init MySQL instance
+mysql = MySQL()
+
+# init RESTful API
+api = Api(app)
+
+# setting DB credentials
+app.config["MYSQL_DATABASE_USER"] = getuser("Username: ")
+app.config["MYSQL_DATABASE_PASSWORD"] = getpass("Password: ")
+app.config["MYSQL_DATABASE_DB"] = "sp500_db"
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
+
+# init MySQL extension
+mysql.init_app(app)
